9b6fe7a766db0b97ee22dad301f3876c3a65fb3f
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2023-11-04 21:09:38 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react"
 import Image from "next/image"
+import Categories from "./components/categories"
 
 export default function Home() {
   return (
@@ -14,6 +15,10 @@ export default function Home() {
         sizes="100vw"
         alt="Até 55% de desconto esse mês"
       />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   )
 }
