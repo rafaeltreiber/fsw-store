89e4e47f27f1f5df620db862ee769090d7ee2379
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2023-11-26 17:15:27 -0300

    ensure user foes to homepage on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -92,7 +92,9 @@ const Header = () => {
       </SheetContent>
     </Sheet>
 
-    <h1 className="font-semibold text-lg"><span className="text-primary">FSW Store</span></h1>
+    <Link href="/">
+      <h1 className="font-semibold text-lg"><span className="text-primary">FSW Store</span></h1>
+    </Link>
 
     <Button size="icon" variant="outline">
       <ShoppingCartIcon />
