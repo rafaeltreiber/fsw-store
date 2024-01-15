0e752e6ad14c537a57d04733b360612046906953
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-01-15 09:26:51 -0300

    feat: add recommended products title to product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
 import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailsPageProps {
   params: {
@@ -36,7 +37,10 @@ const productDetailsPage = async ({ params: { slug } }: ProductDetailsPageProps)
     <div className="flex flex-col gap-8 pb-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
-      <ProductList products={product.category.products} />
+      <div>
+        <SectionTitle>Podutos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   )
 }
