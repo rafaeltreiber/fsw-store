c3784dc53c4bbcac4d6d69caa1e3bae87676861e
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-01-16 10:49:20 -0300

    feat: show total, subtotal and discount on cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,23 +4,59 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { Separator } from "./separator";
 
 const Cart = () => {
-  const { products } = useContext(CartContext)
+  const { products, subTotal, total, totalDiscount } = useContext(CartContext)
   return (
     <div className="flex flex-col gap-8">
       <Badge className="w-fit gap-1 text-base uppercase border-primary px-3 py-[0.375rem] border-2" variant="outline">
         <ShoppingCartIcon size={16} />
-        Catálogo
+        Carrinho
       </Badge>
 
       <div className="flex flex-col gap-5">
-        {products.map(product => (
-          <CartItem
-            key={product.id}
-            product={computeProductTotalPrice(product as any) as any}
-          />
-        ))}
+        {products.length > 0 ? (
+          products.map(product => (
+            <CartItem
+              key={product.id}
+              product={computeProductTotalPrice(product as any) as any}
+            />
+          ))
+        ) : (
+          <p className="text-center font-semibold">Carrinho vazio. Vamos fazer compras?</p>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Separator />
+
+        <div className="flex items-center justify-between">
+          <p>Subtotal</p>
+          <p>R$ {subTotal.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between">
+          <p>Entrega</p>
+          <p>Grátis</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between">
+          <p>Descontos</p>
+          <p>- R$ {totalDiscount.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between font-bold">
+          <p>Total</p>
+          <p>R$ {total.toFixed(2)}</p>
+        </div>
+
       </div>
     </div>
   );
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { productWithTotalPrice } from "@/helpers/product";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useMemo, useState } from "react";
 
 export interface CartProduct extends productWithTotalPrice {
   quantity: number;
@@ -12,6 +12,9 @@ interface ICartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
+  total: number;
+  subTotal: number;
+  totalDiscount: number;
   addProductToCart: (Product: CartProduct) => void;
   decreaseProductQuantity: (ProductId: string) => void;
   increaseProductQuantity: (ProductId: string) => void;
@@ -23,6 +26,9 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
+  total: 0,
+  subTotal: 0,
+  totalDiscount: 0,
   addProductToCart: () => { },
   decreaseProductQuantity: () => { },
   increaseProductQuantity: () => { },
@@ -31,6 +37,20 @@ export const CartContext = createContext<ICartContext>({
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([])
+
+  const subTotal = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.totalPrice)
+    }, 0)
+  }, [products])
+
+  const total = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.basePrice)
+    }, 0)
+  }, [products])
+
+  const totalDiscount = total - subTotal
 
   const addProductToCart = (product: CartProduct) => {
     const productIsAlreadyOnCart = products.some(cartProduct => cartProduct.id === product.id)
@@ -91,6 +111,9 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       decreaseProductQuantity,
       increaseProductQuantity,
       removeProductFromCart,
+      total,
+      subTotal,
+      totalDiscount,
       cartTotalPrice: 0,
       cartBasePrice: 0,
       cartTotalDiscount: 0
